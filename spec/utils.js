4fffad8d59bc55f1fecb7910837787e536c233d3
nimblefox <39785177+nimblefox@users.noreply.github.com>
2018-06-23 18:32:54 -0700

    fix issue #35 (#36)
    Adding utils.js unit tests by @nimblefox 
    Thank you for your assistance in this project! I really appreciate it.

--- spec/utils.js
+++ spec/utils.js
@@ -24,3 +24,53 @@ describe("getContainerLength", function () {
 	});
 });
 
+describe("assert", function () {
+	var func = [
+		'argType',
+		'string',
+		'number',
+		'boolean',
+		'function',
+		'object',
+		'array',
+		'container'
+	];
+
+	it("should have have thunks defined on assert", function () {
+		func.forEach(function(elem, index) {
+			expect(d.utils.assert[elem]).not.toBe(undefined);
+		});
+	});
+});
+
+describe("isObject", function () {
+	it("should return true", function () {
+		expect(d.utils.isObject({})).toEqual(true);
+	});
+
+	it("should return false", function() {
+		expect(d.utils.isObject([])).toEqual(false);
+		expect(d.utils.isObject()).toEqual(false);
+	});
+});
+
+describe("isPrimitive", function () {
+	var primitives = ["string", false, 4, Symbol("prop")];
+	it("should return true for all valid primitives", function () {
+		primitives.forEach(function (elem) {
+			expect(d.utils.isPrimitive(elem)).toEqual(true);
+		});
+	});
+});
+
+describe("createContainer", function () {
+	it('should create an array', function () {
+		var isArr = d.utils.createContainer([]);
+		expect(isArr).toEqual([]);
+	});
+
+	it('should create an object', function () {
+		var isObj = d.utils.createContainer({});
+		expect(isObj).toEqual({});
+	});
+});
