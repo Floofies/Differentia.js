da2fa8743739907181d473c067577823fa9d9aed
Floofies <fantasticdragons@gmail.com>
2018-07-14 23:37:07 -0400

    Remove BinaryTree and RedBlackTree (moving to experimental branch)

--- src/structs.js
+++ src/structs.js
@@ -2,12 +2,10 @@ const OffsetArrayStruct = require('./structs/OffsetArray');
 
 module.exports = {
 	ArrayList: require('./structs/ArrayList'),
-	BinaryTree: require('./structs/BinaryTree'),
 	CircularDoubleLinkedList: require('./structs/CircularDoubleLinkedList'),
 	CircularLinkedList: require('./structs/CircularLinkedList'),
 	DoubleLinkedList: require('./structs/DoubleLinkedList'),
 	LinkedList: require('./structs/LinkedList'),
-	RedBlackTree: require('./structs/RedBlackTree'),
 	OffsetArray: OffsetArrayStruct.OffsetArray,
 	Queue: OffsetArrayStruct.Queue,
 	Stack: OffsetArrayStruct.Stack
